fbabe55ad9e6dc85c3151f4f9187dad5ccb1ebd5
chavalk <chavalk@hotmail.com>
2021-09-10 17:05:58 -0500

    Add start function to create doodler only when the game has started.

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodletLeftSpace = 50
+    let doodlerBottomSpace = 150
+    let isGameOver = false
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
         doodler.style.left = doodletLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
     }
-    createDoodler()
+    
+    function start() {
+        if (!isGameOver) {
+            createDoodler()
+        }
+    }
+    // Attach to a button
+    start()
 })
