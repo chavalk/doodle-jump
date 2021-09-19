264d0a47fe5fc1455fedf397192a591b221d208b
chavalk <chavalk@hotmail.com>
2021-09-19 15:24:36 -0500

    Rearrange variables and add speed and gravity variables.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
+    let isGameOver = false
+    let speed = 3
+    let platformCount = 5
+    let platforms = []
+    let score = 0
     let doodlerLeftSpace = 50
     let startPoint = 150
     let doodlerBottomSpace = startPoint
-    let isGameOver = false
-    let platformCount = 5
-    let platforms = []
+    const gravity = 0.9
     let upTimerId
     let downTimerId
     let isJumping = true
@@ -14,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGoingRight = false
     let leftTimerId
     let rightTimerId
-    let score = 0
+    
 
     function createDoodler() {
         grid.appendChild(doodler)
